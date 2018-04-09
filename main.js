a4dd58231e7c111c29a9c8646a49d1494f82aa86
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-04-09 14:35:44 -0400

    created the page link section and added an event listener to each link

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 
+const pageDiv = document.querySelector('.page');
 const studentList = document.querySelectorAll(".student-item");
 const studentsPerPage = 10;
+const numberOfPages = Math.ceil(studentList.length/studentsPerPage); //determines how many pages are needed
+//page link section
+const paginationDiv = document.createElement('div');
+paginationDiv.className = 'pagination';
+const ul = document.createElement('ul');
+pageDiv.appendChild(paginationDiv);
 
 
 
@@ -18,17 +25,35 @@ function showPage(pageNumber, studentList) {
      }
    }
  }
-showPage(3, studentList);
 
 
-function appendPageLinks(studentList/* take a student list as an argument */) {
-    const numberOfPages = Math.ciel(studentList.length/10);// determine how many pages for this student list
-        // create a page link section
-    // “for” every page
-        // add a page link to the page link section
+
+function appendPageLinks(studentList) {
+
+      // “for” every page
+          // add a page link to the page link section
+      for (let i = 1; i <= numberOfPages; i++) {
+        const li = document.createElement('li');
+        const anchor = document.createElement('a');
+        anchor.href = "#";
+        anchor.text = [i];
+        li.appendChild(anchor);
+        ul.appendChild(li);
+        paginationDiv.appendChild(ul);
+        // adds an event listener to each link created
+        ul.addEventListener('click', (e) => {
+          if (e.target == anchor) {
+            console.log('hi');
+          }
+        });
+    }
+
     // remove the old page link section from the site
     // append our new page link section to the site
     // define what happens when you click a link
         // Use the showPage function to display the page for the link clicked
         // mark that link as “active”
 }
+
+showPage(1, studentList);
+appendPageLinks(studentList);
