e5f6083ffcda18e0bdf723b25ffe9f0c5cf339a3
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-04-12 17:27:36 -0400

    added code to toggle the active class on each link and tested code on different size student lists

--- main.js
+++ main.js
@@ -13,12 +13,12 @@ pageDiv.appendChild(paginationDiv);
 // // This  function displays a "page" or a list of ten students based which "page number" the user selected.
 function showPage(pageNumber, studentList) {
   //these variables give a range of students we want displayed per specific page number
-  const upper = studentsPerPage * pageNumber;
-  const lower = upper - studentsPerPage;
+  const upperNum = studentsPerPage * pageNumber;
+  const lowerNum = upperNum - studentsPerPage;
 
    for (let i = 0; i < studentList.length; i++) {
      studentList[i].style.display = "none"; // first hide all students on the page
-     if ((i >= lower) && (i < upper)) { // if student should be on this page number
+     if ((i >= lowerNum) && (i < upperNum)) { // if student should be on this page number
        studentList[i].style.display = "block"; 	// show the student
      }
    }
@@ -34,15 +34,17 @@ function appendPageLinks(studentList) {
         anchor.text = [i];
         li.appendChild(anchor);
         ul.appendChild(li);
-        paginationDiv.appendChild(ul);
         // adds an event listener to each link created
         ul.addEventListener('click', (e) => {
           if (e.target === anchor) {
             showPage(e.target.text, studentList); //calls the showpage function and determines the page number by using the anchor tag's text;
-            e.target.className = 'active';
+            //toggles 'active' class
+            $('.pagination a').removeClass();
+            $(e.target).addClass('active');
           }//end of if statement
         }); //end of eventListener
     }//end of for loop
+    paginationDiv.appendChild(ul);
 }
 
 showPage(1, studentList); // When the page loads, hide all but the first 10 students in the list.
