e0887962d637de5936c6b99476d00192c9bcb8b9
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-04-13 00:02:24 -0400

    cleaned up the code slightly and added the class of 'active' to the first link upon page load

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ function showPage(pageNumber, studentList) {
      studentList[i].style.display = "none"; // first hide all students on the page
      if ((i >= lowerNum) && (i < upperNum)) { // if student should be on this page number
        studentList[i].style.display = "block"; 	// show the student
-     }
-   }
+     } //end if statement
+   } //end for loop
  }
 
 function appendPageLinks(studentList) {
@@ -24,6 +24,7 @@ function appendPageLinks(studentList) {
   const paginationDiv = document.createElement('div');
   paginationDiv.className = 'pagination';
   const ul = document.createElement('ul');
+  paginationDiv.appendChild(ul);
   pageDiv.appendChild(paginationDiv);
         // for every page
           // add a page link to the page link section
@@ -34,17 +35,22 @@ function appendPageLinks(studentList) {
         link.text = [i];
         li.appendChild(link);
         ul.appendChild(li);
+
+        // will set the first link's class to "active" upon page load
+        if (link.text == '1') {
+          link.classList.add('active');
+        }
+
         // adds an event listener to each link created
         ul.addEventListener('click', (e) => {
           if (e.target === link) {
             showPage(e.target.text, studentList); //calls the showpage function and determines the page number by using the anchor tag's text;
-            //toggles 'active' class
-            $('.pagination a').removeClass();
-            $(e.target).addClass('active');
+            //toggles 'active' class as links are clicked
+            document.querySelectorAll('.pagination a').forEach( el => el.classList.remove('active'));
+            e.target.classList.add('active');
           }//end of if statement
         }); //end of eventListener
     }//end of for loop
-    paginationDiv.appendChild(ul);
 }
 
 showPage(1, studentList); // When the page loads, hide all but the first 10 students in the list.
