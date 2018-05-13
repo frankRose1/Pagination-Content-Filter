78e457d7cf08715dbddc64c052846ff91ef95845
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-05-12 21:45:13 -0400

    did some refactoring

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Techdegree Project 2
+# Techdegree Project 2 - Pagination
+# Using Javascript to create pagination based on a list of students
+# students are displayed on the page based on their appropriate page number
--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-
 const pageDiv = document.querySelector('.page');
 const studentList = document.querySelectorAll(".student-item");
 const studentsPerPage = 10;
 const numberOfPages = Math.ceil(studentList.length/studentsPerPage); //determines how many pages are needed
 
-
-// // This  function displays a "page" or a list of ten students based which "page number" the user selected.
-function showPage(pageNumber, studentList) {
+// // This  function displays a "page" or a list of ten students based which page number the user selected.
+function showPage(pageNumber) {
   //these variables give a range of students we want displayed per specific page number
   const upperNum = studentsPerPage * pageNumber;
   const lowerNum = upperNum - studentsPerPage;
 
-   for (let i = 0; i < studentList.length; i++) {
-     studentList[i].style.display = "none"; // first hide all students on the page
-     if ((i >= lowerNum) && (i < upperNum)) { // if student should be on this page number
-       studentList[i].style.display = "block"; 	// show the student
-     } //end if statement
-   } //end for loop
+  studentList.forEach( (student, i) => {
+    student.style.display = 'none';
+
+    // if student should be on this page number
+    if ( (i >= lowerNum) && (i < upperNum) ) {
+       student.style.display = "block";  // show the student
+     } 
+  });
  }
 
-function appendPageLinks(studentList) {
-  //page link section
+function appendPageLinks() {
+ 
   const paginationDiv = document.createElement('div');
-  paginationDiv.className = 'pagination';
+  paginationDiv.classList.add('pagination');
   const ul = document.createElement('ul');
   paginationDiv.appendChild(ul);
   pageDiv.appendChild(paginationDiv);
+
         // for every page
           // add a page link to the page link section
       for (let i = 1; i <= numberOfPages; i++) {
@@ -41,17 +42,21 @@ function appendPageLinks(studentList) {
           link.classList.add('active');
         }
 
-        // adds an event listener to each link created
-        ul.addEventListener('click', (e) => {
-          if (e.target === link) {
-            showPage(e.target.text, studentList); //calls the showpage function and determines the page number by using the anchor tag's text;
-            //toggles 'active' class as links are clicked
-            document.querySelectorAll('.pagination a').forEach( el => el.classList.remove('active'));
-            e.target.classList.add('active');
-          }//end of if statement
-        }); //end of eventListener
-    }//end of for loop
+        activeLink(link);
 }
 
+ function activeLink(link) {
+     // adds an event listener to each link created
+        ul.addEventListener('click', (e) => {
+          if (e.target === link) {
+            showPage(e.target.text); //calls the showpage function and determines the page number by using the anchor tag's text;
+            //toggles 'active' class as links are clicked
+            document.querySelectorAll('.pagination a').forEach( link => link.classList.remove('active'));
+            e.target.classList.add('active');
+          } //end if statement
+        }); //end eventListener
+    } //end for loop
+ }
+
 showPage(1, studentList); // When the page loads, hide all but the first 10 students in the list.
-appendPageLinks(studentList);
+appendPageLinks();
