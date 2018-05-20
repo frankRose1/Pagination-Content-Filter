fae14977194b81f4d5356fd49eee8642955b63f8
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-05-19 21:58:12 -0400

    refactored the activeLink function making it more concise and cleaned up the comments

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const numberOfPages = Math.ceil(studentList.length/studentsPerPage); //determine
 
 // // This  function displays a "page" or a list of ten students based which page number the user selected.
 function showPage(pageNumber) {
-  //these variables give a range of students we want displayed per specific page number
+  //these variables give a range of students we want displayed per page number
   const upperNum = studentsPerPage * pageNumber;
   const lowerNum = upperNum - studentsPerPage;
 
@@ -19,6 +19,7 @@ function showPage(pageNumber) {
   });
  }
 
+//create and append page links 
 function appendPageLinks() {
  
   const paginationDiv = document.createElement('div');
@@ -26,9 +27,11 @@ function appendPageLinks() {
   const ul = document.createElement('ul');
   paginationDiv.appendChild(ul);
   pageDiv.appendChild(paginationDiv);
-
+    
+    
+    
         // for every page
-          // add a page link to the page link section
+          // add a page link to the pagination Div
       for (let i = 1; i <= numberOfPages; i++) {
         const li = document.createElement('li');
         const link = document.createElement('a');
@@ -37,25 +40,24 @@ function appendPageLinks() {
         li.appendChild(link);
         ul.appendChild(li);
 
-        // will set the first link's class to "active" upon page load
+        // will set the first link's class to "active" by default
         if (link.text == '1') {
           link.classList.add('active');
         }
 
         activeLink(link);
+      } //end of for loop
 }
 
+//show the relevant page and toggle the 'active class'    
  function activeLink(link) {
-     // adds an event listener to each link created
-        ul.addEventListener('click', (e) => {
-          if (e.target === link) {
-            showPage(e.target.text); //calls the showpage function and determines the page number by using the anchor tag's text;
-            //toggles 'active' class as links are clicked
-            document.querySelectorAll('.pagination a').forEach( link => link.classList.remove('active'));
-            e.target.classList.add('active');
-          } //end if statement
-        }); //end eventListener
-    } //end for loop
+     link.addEventListener('click', (e) => {
+         //show the page determined by the target link's text
+         showPage(e.target.text);
+         //toggle 'active' class as links are clicked
+         document.querySelectorAll('.pagination a').forEach( link => link.classList.remove('active'));
+         e.target.classList.add('active');
+     });
  }
 
 showPage(1, studentList); // When the page loads, hide all but the first 10 students in the list.
