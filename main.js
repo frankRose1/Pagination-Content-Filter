533695526f3d4236791dee8ca0f75e8dae6dcc82
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-05-22 23:03:43 -0400

    began adding search bar functionality

--- main.js
+++ main.js
@@ -3,15 +3,14 @@ const studentList = document.querySelectorAll(".student-item");
 const studentsPerPage = 10;
 const numberOfPages = Math.ceil(studentList.length/studentsPerPage); //determines how many pages are needed
 
-// // This  function displays a "page" or a list of ten students based which page number the user selected.
+//display a "page" or a list of ten students based which page number the user selected.
 function showPage(pageNumber) {
-  //these variables give a range of students we want displayed per page number
+  //give a range of students we want displayed per page number
   const upperNum = studentsPerPage * pageNumber;
   const lowerNum = upperNum - studentsPerPage;
 
   studentList.forEach( (student, i) => {
     student.style.display = 'none';
-
     // if student should be on this page number
     if ( (i >= lowerNum) && (i < upperNum) ) {
        student.style.display = "block";  // show the student
@@ -20,7 +19,7 @@ function showPage(pageNumber) {
  }
 
 //create and append page links 
-function appendPageLinks() {
+function appendPageLinks(numOfPages) {
  
   const paginationDiv = document.createElement('div');
   paginationDiv.classList.add('pagination');
@@ -28,10 +27,8 @@ function appendPageLinks() {
   paginationDiv.appendChild(ul);
   pageDiv.appendChild(paginationDiv);
     
-    
-    
-        // for every page
-          // add a page link to the pagination Div
+//         for every page
+//           add a page link to the pagination Div
       for (let i = 1; i <= numberOfPages; i++) {
         const li = document.createElement('li');
         const link = document.createElement('a');
@@ -61,4 +58,37 @@ function appendPageLinks() {
  }
 
 showPage(1, studentList); // When the page loads, hide all but the first 10 students in the list.
-appendPageLinks();
+appendPageLinks(numberOfPages);
+
+
+//SEARCH BUTTON BEGIN 
+//create and append a search component to sort through a list of students
+//when search button is clicked us the value to display student names/emails that match the input value
+
+// /* <div class="student-search">
+//           <input placeholder="Search for students...">
+//           <button>Search</button>
+//   </div> */
+
+const pageHeader = document.querySelector('.page-header');
+
+//create the necessary elements
+const searchDiv = document.createElement('div');
+searchDiv.classList.add('student-search');
+const input = document.createElement('input');
+input.type = 'text';
+input.placeholder = 'Search for students...';
+const searchButton = document.createElement('button');
+searchButton.innerHTML = 'Search'
+searchDiv.appendChild(input);
+searchDiv.appendChild(searchButton);
+
+pageHeader.appendChild(searchDiv);
+
+//convert the student list from a node lise to an array
+const studentDetails = document.querySelectorAll('.student-details');
+const newArr = Array.from(studentDetails);
+const studentData = newArr
+                .map(student => student.children)
+                .map(child => child);
+console.log(studentData);
