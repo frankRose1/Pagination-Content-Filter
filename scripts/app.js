fb0a3bc45b2e8c874c064fe79b7be17500e0cc1a
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-06-20 23:46:08 -0400

    calculated how to show the correct number of students based on the page number

--- scripts/app.js
+++ scripts/app.js
@@ -16,19 +16,24 @@ studentList.forEach(student => { //hide all of the students
 });
 
 function pagination( page = 1, list = studentList){
-    //we have a nodeList and we only want to display a certain number of the students based on a page
+   
     const limit = 10; //students per page
-    const pagesNeeded = Math.ceil(list / limit);
-    const skip = (page * limit) - limit;
+    const maxPages = Math.ceil(list.length / limit);
+    const maxIndex = limit * maxPages;
+    const skipIndex = (page * limit) - limit;
                 // 1  *   10   -  10   -> we skip 0( show the first 10 )
                 // 2 *     10  -   10  ->  we skip 10 (first 10 are skipped) how do we hide everything after 20?
-    //now we need an upper value
-    
+
+        // the second calucaltion is the limit scaling based on the page number subtracted from the max value
+        //so if page number is 3 then that calc = 30, and upper equals 30
+    const upper = maxIndex - ( maxIndex - (limit * page) );
     //loop over the list and see where the students land
     list.forEach( (el, i) => {
-        //if the index is greater than the skip then we show the student
-        if ( i >= skip ) {
+        //if the index is greater than the skip, and lower than the upper value then we show the student, else we hide them
+        if ( i >= skipIndex && i < upper) {
             el.style.display = 'block';
+        } else {
+            el.style.display = 'none'
         }
     });// end loop
 
